203807dd9619d7d0895de1ac01cbdc9965ebc6ec
Emil Gram <hi@emilgram.dk>
2025-05-23 11:11:55 +0200

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -4,3 +4,4 @@ export { replaceKeys } from "./utils/replaceKeys";
 export { throttle } from "./utils/throttle";
 export { debounce } from "./utils/debounce";
 export { ping } from "./utils/ping";
+export { AbortableAPIService } from "./utils/api-service";
